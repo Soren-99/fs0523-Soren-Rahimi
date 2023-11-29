db735a50dc6b9fda1a19365b3fa9244a9bc65b22
Persiano99 <sorenrahimi99@gmail.com>
2023-11-29 22:53:51 +0100

    problemi tecnici-da finire-m3-s2-g3

---
+++ m3/s2/g3/pg3/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ActiveComponent } from './pages/active/active.component';
+import { InactiveComponent } from './pages/inactive/inactive.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ActiveComponent,
+    InactiveComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
