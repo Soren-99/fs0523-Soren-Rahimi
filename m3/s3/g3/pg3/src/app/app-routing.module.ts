546cccc2393722fd1ff0fb4dd98e7fbf0e0fa858
Persiano99 <sorenrahimi99@gmail.com>
2023-12-08 20:49:28 +0100

    pratica-m3-g3

---
+++ m3/s3/g3/pg3/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  { path: 'chi-siamo', loadChildren: () => import('./pages/chi-siamo/chi-siamo.module').then(m => m.ChiSiamoModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
